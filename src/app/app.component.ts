8867aec4a391f67ebaa5f7a9fe4ae256707a3049
Dallas James <dallastjames@gmail.com>
2020-11-04 14:25:59 -0700

    init to win it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,24 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicAuth } from '@ionic-enterprise/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
   ) {
     this.initializeApp();
+    const auth = new IonicAuth({
+      clientID: '',
+      logoutUrl: '',
+    });
   }
 
   initializeApp() {
